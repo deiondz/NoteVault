72d44a808a028f158299d13e0ab909a4d1b52c7c
DD <64969704+deiondz@users.noreply.github.com>
2023-12-23 01:02:50 +0300

    Added fix

---
+++ components/Card.jsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+
+export default function Card(props) {
+  return (
+    <Link
+      href={props.e.href}
+      className="flex hover:ring-2 cursor-pointer flex-col w-full md:w-[350px] lg:w-[400px] h-full brightness-90 hover:brightness-100 transition-all bg-[#242C2F] p-10 rounded-lg aspect-video items-start space-y-4"
+    >
+      <h1 className="text-3xl font-extrabold tracking-[-1%]">
+        {props.e.title}
+      </h1>
+      <p className="font-semibold ">Information :</p>
+      <ul className="space-y-2 list-disc list-inside">
+        {props.e.courses.map((e, index) => (
+          <li
+            key={index}
+            className="font-normal  text-[#BEB8B8]  text-xs md:text-sm "
+          >
+            {e.name}
+          </li>
+        ))}
+      </ul>
+    </Link>
+  );
+}
